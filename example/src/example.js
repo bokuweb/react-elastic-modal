339f8722346552d2dcecd2617362122a85265569
bokuweb <bokuweb12@gmail.com>
2016-02-22 15:15:06 +0900

    init

---
+++ example/src/example.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import HelloWorld from '../../src';
+
+export default class Example extends Component{
+  render() {
+    return (
+      <HelloWorld />
+    );
+  }
+}
