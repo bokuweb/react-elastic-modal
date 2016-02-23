d2d3d23e9637a72bab746d0ee3cf122efbb64e32
bokuweb <bokuweb12@gmail.com>
2016-02-23 16:28:20 +0900

    Remove mount state

--- src/index.js
+++ src/index.js
@@ -3,31 +3,25 @@ import React, { Component } from 'react';
 const ease = 1.5;
 const friction = 0.95;
 
-export default class HelloWorld extends Component {
+export default class ElasticModal extends Component {
   constructor(props) {
     super(props);
     this.velocity = 0;
     this.state = {
-      isMount: false,
       isOpen: false,
       x: -40,
     };
   }
 
   componentDidMount() {
-    console.log(this.refs.wrapper.clientWidth)
-    console.log(this.refs.wrapper.clientHeight)
-    this.setState({
-      isMount: true,
-    });
     this.tick();
   }
 
   spring(destination, position) {
-    //if (this.state.isOpen) return destination;
+    if (this.state.isOpen) return destination;
     this.velocity += (destination - position) * ease;
     this.velocity *= friction;
-    //if (destination - position < 0.01) this.setState({ isOpen: true });
+    if (Math.abs(this.velocity) < 0.5) this.setState({ isOpen: true });
     return position + this.velocity;
   };
 
@@ -37,14 +31,11 @@ export default class HelloWorld extends Component {
     requestAnimationFrame(this.tick.bind(this));
   }
 
-  renderBox() {
-    
-  }
-
   render() {
     const { style } = this.props;
     return (
       <div ref='wrapper' style={ Object.assign({}, style, { position: 'fixed' }) }>
+        {/* TODO: if is open change svg props to `width: 100%, height: 100%, top:0, left:0` */}
         <svg width="120%" height="120%" style={{ position: 'absolute', top: '-10%', left: '-10%'}}>
   	      <path d={`M 10 10
                     Q 60 ${ this.state.x } 110 10
