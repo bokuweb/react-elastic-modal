e0c32d16d914725e655c4f11fc913f8dda4adebd
bokuweb <bokuweb12@gmail.com>
2016-02-26 23:09:09 +0900

    Implement size adjuster

--- example/src/example.js
+++ example/src/example.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 import HelloWorld from '../../src';
 
+const customStyles = {
+  content : {
+    width: '200px',
+    height: '200px',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  }
+};
+
 export default class Example extends Component{
   render() {
     return (
-      <HelloWorld style={{ width: '100px', height: '100px' }}/>
+      <HelloWorld style={ customStyles.content }/>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 
-const ease = 1.5;
-const friction = 0.95;
+const ease = 1;
+const friction = 0.85;
 
 export default class ElasticModal extends Component {
   static propTypes = {
@@ -13,39 +13,55 @@ export default class ElasticModal extends Component {
     this.velocity = 0;
     this.state = {
       isOpen: false,
-      x: -40,
+      x: -100,
     };
   }
 
   componentDidMount() {
     this.tick();
+    const { wrapper } = this.refs;
+    this.setState({
+      height: wrapper.clientHeight,
+      width: wrapper.clientWidth,
+    });
+  }
+
+  componentWillReceiveProps(next) {
+    if (next.isOpen) {
+
+    }
   }
 
   spring(destination, position) {
     if (this.state.isOpen) return destination;
     this.velocity += (destination - position) * ease;
     this.velocity *= friction;
-    if (Math.abs(this.velocity) < 0.5) this.setState({ isOpen: true });
+    //if (Math.abs(this.velocity) < 0.5) this.setState({ isOpen: true });
     return position + this.velocity;
   }
 
   tick() {
-    const x = this.spring(10, this.state.x);
+    const x = this.spring(20, this.state.x);
     this.setState({ x });
     requestAnimationFrame(this.tick.bind(this));
   }
 
   render() {
     const { style } = this.props;
+    const { width, height } = this.state;
+    const org = {
+      x: width * 0.1,
+      y: height * 0.1,
+    };
     return (
-      <div ref="wrapper" style={ Object.assign({}, style, { position: 'fixed' }) }>
+      <div ref="wrapper" style={ Object.assign({}, { position: 'fixed' }, style) }>
         {/* TODO: if is open change svg props to `width: 100%, height: 100%, top:0, left:0` */}
         <svg width="120%" height="120%" style={{ position: 'absolute', top: '-10%', left: '-10%' }}>
-          <path d={ `M 10 10
-                     Q 60 ${ this.state.x } 110 10
-                     Q 110 60 110 110
-                     Q 60 110 10 110
-                     Q 10 60 10 10 ` }
+          <path d={ `M ${org.x} ${org.y}
+                     Q ${width / 2 + org.x} ${ this.state.x } ${width + org.x} ${org.y}
+                     Q ${width + org.x} ${height / 2 + org.y} ${width + org.x} ${height + org.y}
+                     Q ${width / 2 + org.x} ${height + org.y} ${org.x} ${height + org.y}
+                     Q ${org.x} ${height / 2 + org.y} ${org.x} ${org.y} ` }
             fill="#ddd"
           />
         </svg>
