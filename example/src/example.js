5ad96866384386e491b3a67f5560a86946637997
bokuweb <bokuweb12@gmail.com>
2016-03-05 21:01:58 +0900

    Fix example and screenshot

--- example/src/example.js
+++ example/src/example.js
@@ -16,7 +16,7 @@ export default class Example extends Component {
           onRequestClose={ () => this.setState({ isOpen: false }) }
           modal={{
             width: '50%',
-            height: '420px',
+            height: '360px',
             backgroundColor: '#fff',
             opacity: 0.5,
           }}
@@ -25,7 +25,7 @@ export default class Example extends Component {
           }}
         >
           <div style={{ padding: '50px', overflowY: 'scroll', position: 'relative' }}>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <div className="button--close" onClick={ () => this.setState({ isOpen: false }) }>close modal</div>
             <i
                className="fa fa-close"
